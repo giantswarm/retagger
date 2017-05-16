26d16d559e415f69cd1a27e79e59d47cb26e4d54
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-05-16 16:34:41 +0100

    Removes unnecessary newlines from logging

--- main.go
+++ main.go
@@ -9,32 +9,32 @@ import (
 func main() {
 	registry := os.Getenv("REGISTRY")
 	if registry == "" {
-		log.Fatalf("could not get registry\n")
+		log.Fatalf("could not get registry")
 	}
 
 	registryOrganisation := os.Getenv("REGISTRY_ORGANISATION")
 	if registryOrganisation == "" {
-		log.Fatalf("could not get registry organisation\n")
+		log.Fatalf("could not get registry organisation")
 	}
 
 	registryUsername := os.Getenv("REGISTRY_USERNAME")
 	if registryUsername == "" {
-		log.Fatalf("could not get registry username\n")
+		log.Fatalf("could not get registry username")
 	}
 
 	registryPassword := os.Getenv("REGISTRY_PASSWORD")
 	if registryPassword == "" {
-		log.Fatalf("could not get registry password\n")
+		log.Fatalf("could not get registry password")
 	}
 
 	login := exec.Command("docker", "login", "--email=\" \"", "-u", registryUsername, "-p", registryPassword, registry)
 	if err := Run(login); err != nil {
-		log.Fatalf("could not login to registry: %v\n", err)
+		log.Fatalf("could not login to registry: %v", err)
 	}
 
 	for _, image := range Images {
 		for _, tag := range image.Tags {
-			log.Printf("managing: %v, %v, %v\n", image.Name, tag.Sha, tag.Tag)
+			log.Printf("managing: %v, %v, %v", image.Name, tag.Sha, tag.Tag)
 
 			retaggedName := RetaggedName(registry, registryOrganisation, image)
 			shaName := ShaName(image.Name, tag.Sha)
@@ -44,28 +44,28 @@ func main() {
 			log.Printf("checking if image has already been retagged")
 			pullRetag := exec.Command("docker", "pull", retaggedNameWithTag)
 			if err := Run(pullRetag); err == nil {
-				log.Printf("retagged image already exists, skipping\n")
+				log.Printf("retagged image already exists, skipping")
 				continue
 			} else {
-				log.Printf("retagged image probably does not exist: %v\n", err)
+				log.Printf("retagged image probably does not exist: %v", err)
 			}
 
 			log.Printf("pulling original image")
 			pullOriginal := exec.Command("docker", "pull", shaName)
 			if err := Run(pullOriginal); err != nil {
-				log.Fatalf("could not pull image: %v\n", err)
+				log.Fatalf("could not pull image: %v", err)
 			}
 
 			log.Printf("retagging image")
 			retag := exec.Command("docker", "tag", shaName, retaggedNameWithTag)
 			if err := Run(retag); err != nil {
-				log.Fatalf("could not retag image: %v\n", err)
+				log.Fatalf("could not retag image: %v", err)
 			}
 
 			log.Printf("pushing image")
 			push := exec.Command("docker", "push", retaggedNameWithTag)
 			if err := Run(push); err != nil {
-				log.Fatalf("could not push image: %v\n", err)
+				log.Fatalf("could not push image: %v", err)
 			}
 		}
 	}
