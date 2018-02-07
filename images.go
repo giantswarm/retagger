9914e433a625de6b647e3282ae7f82f730fb03cb
Julien Garcia Gonzalez <garciagonzalez.julien@gmail.com>
2018-02-07 11:14:36 +0100

    add new version of fluentd (#49)
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

--- images.go
+++ images.go
@@ -57,6 +57,15 @@ var Images = []Image{
 		},
 	},
 	Image{
+		Name: "gcr.io/google-containers/fluentd-elasticsearch",
+		Tags: []Tag{
+			Tag{
+				Sha: "0bee097b7f7f23c2fc79a1ad39beabe97832b6ceb8e03e12408f16e99ac56d3a",
+				Tag: "v2.0.3",
+			},
+		},
+	},
+	Image{
 		Name: "gcr.io/google_containers/defaultbackend",
 		Tags: []Tag{
 			Tag{
