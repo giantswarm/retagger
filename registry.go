4baaf16c0c9346dbd86262f6e8379ea84fa1b37b
Federico Gimenez <federico.gimenez@gmail.com>
2018-06-29 11:58:37 +0200

    use v2 registry API

--- registry.go
+++ registry.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 
 	"github.com/giantswarm/microerror"
 )
@@ -27,6 +29,14 @@ type Registry struct {
 	password     string
 	token        string
 	username     string
+}
+
+type TagsListResponse struct {
+	Tags []string `json:"tags"`
+}
+
+type TokenRequestResponse struct {
+	Token string `json:"token"`
 }
 
 func NewRegistry(cfg *RegistryConfig) (*Registry, error) {
@@ -75,12 +85,18 @@ func (r *Registry) Login() error {
 }
 
 func (r *Registry) CheckImageTagExists(image, tag string) (bool, error) {
-	url := fmt.Sprintf("https://%s/api/v1/repository/%s/tag/%s/images", r.host, ImageName(r.organisation, image), tag)
+	url := fmt.Sprintf("https://%s/v2/%s/tags/list", r.host, ImageName(r.organisation, image))
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return false, microerror.Mask(err)
 	}
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", r.token))
+	token, err := r.getToken(req)
+	if err != nil {
+		return false, microerror.Mask(err)
+	}
+	if token != "" {
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
 
 	res, err := r.client.Do(req)
 	if err != nil {
@@ -90,7 +106,17 @@ func (r *Registry) CheckImageTagExists(image, tag string) (bool, error) {
 
 	switch res.StatusCode {
 	case http.StatusOK:
-		return true, nil
+		tagResponse := &TagsListResponse{}
+		err = json.NewDecoder(res.Body).Decode(tagResponse)
+		if err != nil {
+			return false, microerror.Mask(err)
+		}
+		for _, imageTag := range tagResponse.Tags {
+			if imageTag == tag {
+				return true, nil
+			}
+		}
+		return false, nil
 	default:
 		log.Printf("could not check retag status: %v", res.StatusCode)
 		return false, nil
@@ -107,4 +133,81 @@ func (r *Registry) Retag(image, sha, tag string) (string, error) {
 		return "", microerror.Mask(err)
 	}
 	return retaggedNameWithTag, nil
+}
+
+func (r *Registry) getToken(req *http.Request) (string, error) {
+	const authorizationHeaderKey = "Www-Authenticate"
+
+	res, err := r.client.Do(req)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+	defer res.Body.Close()
+
+	authorizationHeaderValue := res.Header[authorizationHeaderKey]
+	if len(authorizationHeaderValue) == 0 {
+		// no need for authorization
+		return "", nil
+	}
+
+	authURL, err := getAuthURL(authorizationHeaderValue[0])
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	reqToken, err := http.NewRequest(http.MethodGet, authURL, nil)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+	resToken, err := r.client.Do(reqToken)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+	defer resToken.Body.Close()
+
+	tokenResponse := &TokenRequestResponse{}
+	err = json.NewDecoder(resToken.Body).Decode(tokenResponse)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	return tokenResponse.Token, nil
+}
+
+func getAuthURL(authenticateChallenge string) (string, error) {
+	// www-authenticate headers have this form:
+	// Bearer realm="<realm>",service="<service>"[,scope="<scope>"]
+
+	authenticateChallenge = strings.Replace(authenticateChallenge, `"`, "", -1)
+
+	parts := strings.Fields(authenticateChallenge)
+	if len(parts) < 2 {
+		return "", microerror.Mask(invalidAuthenticateChallengeError)
+	}
+	items := strings.Split(parts[1], ",")
+	if len(items) < 2 {
+		return "", microerror.Mask(invalidAuthenticateChallengeError)
+	}
+	kv := strings.Split(items[0], "=")
+	if len(kv) < 2 {
+		return "", microerror.Mask(invalidAuthenticateChallengeError)
+	}
+	realm := kv[1]
+	kv = strings.Split(items[1], "=")
+	if len(kv) < 2 {
+		return "", microerror.Mask(invalidAuthenticateChallengeError)
+	}
+	service := kv[1]
+	var scope string
+	if len(items) == 3 {
+		kv = strings.Split(items[2], "=")
+		if len(kv) < 2 {
+			return "", microerror.Mask(invalidAuthenticateChallengeError)
+		}
+		scope = kv[1]
+	}
+
+	url := fmt.Sprintf("%s?service=%s&scope=%s", realm, service, scope)
+
+	return url, nil
 }
