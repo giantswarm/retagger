3de19f1823cff1168cd05c293032c1f739d6b782
Zach Stone <zach@giantswarm.io>
2020-05-12 15:04:44 +0200

    Update pkg/images/error.go
    Don't match executionFailedError
    
    Co-authored-by: Paweł Kopiczko <pawel@giantswarm.io>

--- pkg/images/error.go
+++ pkg/images/error.go
@@ -5,8 +5,3 @@ import "github.com/giantswarm/microerror"
 var executionFailedError = &microerror.Error{
 	Kind: "executionFailedError",
 }
-
-// IsExecutionFailed asserts executionFailedError.
-func IsExecutionFailed(err error) bool {
-	return microerror.Cause(err) == executionFailedError
-}
