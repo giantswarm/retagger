1e8321328a7b8e911ea38fc4177d006a23cddf1d
Laszlo Uveges <laszlo@giantswarm.io>
2022-08-29 16:03:48 +0200

    Remove sleep for checking registry

--- integration/test/e2e/e2e_test.go
+++ integration/test/e2e/e2e_test.go
@@ -4,13 +4,11 @@
 package e2e
 
 import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger/microloggertest"
 	"os"
 	"os/exec"
 	"testing"
-	"time"
-
-	"github.com/giantswarm/microerror"
-	"github.com/giantswarm/micrologger/microloggertest"
 
 	"github.com/giantswarm/retagger/pkg/images"
 	"github.com/giantswarm/retagger/pkg/registry"
@@ -48,9 +46,6 @@ func TestE2e(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	t.Logf("waiting %d seconds before checking images in registry...", waitTimeInSecondsBeforeCheckingImagesInRegistry)
-	time.Sleep(waitTimeInSecondsBeforeCheckingImagesInRegistry * time.Second)
 
 	CheckImageDoesNotExistOrFail(t, r, "retagger-e2e", "2.5")
 
